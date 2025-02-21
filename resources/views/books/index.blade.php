08907fac4e2fa424708aa0846524b7eef7ca4945
Khoi <khoin147@gmail.com>
2025-02-21 19:07:23 +0700

    complete popular and highest rated

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -15,8 +15,8 @@
                 '' => 'Latest',
                 'popular_last_month' => 'Popular Last Month',
                 'popular_last_6months' => 'Popular Last 6 Months',
-                'highest_rated_month' => 'Highest Rated last Month',
-                'popular_rated_6months' => 'Highest Rated Last 6 Months'
+                'highest_rated_last_month' => 'Highest Rated last Month',
+                'highest_rated_last_6months' => 'Highest Rated Last 6 Months'
             ]
         @endphp
 
